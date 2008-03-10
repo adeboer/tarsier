56b19ca10b343cac72ea4fcc11bdeb4854284da1
Anthony de Boer <adb@sheepdip.leftmind.net>
2008-03-09 23:01:30 -0400

    allow filename on commandline

--- tarsier.c
+++ tarsier.c
@@ -12,21 +12,38 @@ int main (int argc, char *argv[]) {
 	char buff[4096];
 	mode_t ftype;
 	EVP_MD_CTX mdctx;
+	const EVP_MD *mdtype;
 	unsigned char md_value[EVP_MAX_MD_SIZE];
 	int md_len, i;
+	FILE *fin;
+
+	if (argc == 1) {
+		fin = stdin;
+		}
+	else if (argc == 2) {
+		fin = fopen(argv[1], "r");
+		if (fin == NULL) {
+			perror(argv[1]);
+			exit(1);
+			}
+		}
+	else {
+		fprintf(stderr, "Usage: tarsier %d\n", argc);
+		exit (2);
+		}
 
 	ark = archive_read_new();
 	archive_read_support_compression_all(ark);
 	archive_read_support_format_all(ark);
-
-	rc = archive_read_open_FILE(ark, stdin);
+	rc = archive_read_open_FILE(ark, fin);
 
 	EVP_MD_CTX_init(&mdctx);
+	mdtype = EVP_md5();
 
 	while (archive_read_next_header(ark, &entry) == ARCHIVE_OK) {
 		ftype = archive_entry_filetype(entry);
 		if (S_ISREG(ftype)) {
-			EVP_DigestInit_ex(&mdctx, EVP_md5(), NULL);
+			EVP_DigestInit_ex(&mdctx, mdtype, NULL);
 			while (rc = archive_read_data(ark, buff, sizeof(buff))) {
 				EVP_DigestUpdate(&mdctx, buff, rc);
 				}

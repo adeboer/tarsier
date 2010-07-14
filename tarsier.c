21e07b31e88d53c53759c016361f0ba27f0bff48
Anthony de Boer <adb@dun-bheagan>
2010-07-14 08:05:18 -0400

    first pass at date code

--- tarsier.c
+++ tarsier.c
@@ -2,8 +2,11 @@
 
 #include <stdio.h>
 #include <archive.h>
+#include <archive_entry.h>
 #include <sys/stat.h>
 #include <openssl/evp.h>
+#include <unistd.h>
+#include <time.h>
 
 int main (int argc, char *argv[]) {
 	struct archive *ark;
@@ -12,23 +15,50 @@ int main (int argc, char *argv[]) {
 	char buff[4096];
 	mode_t ftype;
 	EVP_MD_CTX mdctx;
-	const EVP_MD *mdtype;
+	const EVP_MD *mdtype = EVP_md5();
 	unsigned char md_value[EVP_MAX_MD_SIZE];
+	char *dateformat = NULL;
 	int md_len, i;
+	int inopt = 1;
+	int noneyet = 1;
 	FILE *fin;
+	time_t oldest;
+	time_t newest;
 
-	if (argc == 1) {
+	while (inopt) {
+		switch(getopt(argc, argv, "msdD:")) {
+		case -1:
+			inopt = 0;
+			break;
+		case 'm':
+			/* mdtype = EVP_md5(); */
+			break;
+		case 's':
+			mdtype = EVP_sha1();
+			break;
+		case 'd':
+			dateformat = "%c";
+			mdtype = NULL;
+			break;
+		case 'D':
+			dateformat = optarg;
+			mdtype = NULL;
+			break;
+		}
+	}
+
+	if (argc == optind) {
 		fin = stdin;
 		}
-	else if (argc == 2) {
-		fin = fopen(argv[1], "r");
+	else if (argc == optind + 1) {
+		fin = fopen(argv[optind], "r");
 		if (fin == NULL) {
-			perror(argv[1]);
+			perror(argv[optind]);
 			exit(1);
 			}
 		}
 	else {
-		fprintf(stderr, "Usage: tarsier %d\n", argc);
+		fprintf(stderr, "Usage: tarsier [-m|-s|-d|-D fmt] %d\n", argc);
 		exit (2);
 		}
 
@@ -44,12 +74,17 @@ int main (int argc, char *argv[]) {
 		exit(1);
 		}
 
-	EVP_MD_CTX_init(&mdctx);
-	mdtype = EVP_md5();
+	if (mdtype) EVP_MD_CTX_init(&mdctx);
 
 	while ((ast = archive_read_next_header(ark, &entry)) == ARCHIVE_OK) {
 		ftype = archive_entry_filetype(entry);
+		time_t fage = archive_entry_mtime(entry);
+		if (noneyet || fage < oldest) oldest = fage;
+		if (noneyet || fage > newest) newest = fage;
+		noneyet = 0;
+		char *space = "";
 		if (S_ISREG(ftype)) {
+			if (mdtype) {
 			if (EVP_DigestInit_ex(&mdctx, mdtype, NULL) == 0) {
 				fprintf(stderr, "EVP_DigestInit_ex failed\n");
 				exit(1);
@@ -72,13 +107,26 @@ int main (int argc, char *argv[]) {
 				exit(1);
 				}
 			for(i = 0; i < md_len; i++) printf("%02x", md_value[i]);
-			printf("  %s\n",archive_entry_pathname(entry));
+			space = "  ";
+			} else {
+				archive_read_data_skip(ark);
 			}
+			}
+			if (*space) printf("%s%s\n", space, archive_entry_pathname(entry));
 		}
 	if (ast != ARCHIVE_EOF) {
 		fprintf(stderr, "Error parsing archive: %s\n", archive_error_string(ark));
 		exit(1);
 		}
 	archive_read_finish(ark);
-	EVP_MD_CTX_cleanup(&mdctx);
+	if (mdtype) EVP_MD_CTX_cleanup(&mdctx);
+	if (dateformat != NULL && !noneyet) {
+		struct tm * tmp = localtime(&newest);
+		char out[256];
+		if (strftime(out, sizeof(out), dateformat, tmp) == 0) {
+			fprintf(stderr, "date format conversion failure");
+			exit(1);
+		}
+		printf("%s\n", out);
 	}
+}
